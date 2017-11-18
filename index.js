4c0bfebb156d366b01588e9f71fa3956fdc3aa6c
Jennifer Haddock <jentx123@hotmail.com>
2017-11-18 23:27:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here:
+function driversWithRevenueOver(array, revenue){
+  return array.filter(function(matchIt){
+    return matchIt.revenue >= revenue
+  })
+}
+
+function driverNamesWithRevenueOver(array, revenue){
+  return driversWithRevenueOver(array, revenue).map(name => name.revenue > revenue)
+}
+
+function exactMatch(array, revenue){
+  return array.filter(function(matchIt){
+    return matchIt.revenue === revenue
+  })
+}
